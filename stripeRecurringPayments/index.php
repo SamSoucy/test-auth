7bb3c3399b61bd85ffcbe9280674c0fe1e53ec41
Samsoucy <newf930@outlook.com>
2019-07-21 15:28:40 -0400

    moved paths

--- stripeRecurringPayments/index.php
+++ stripeRecurringPayments/index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.auth0.com/js/auth0-spa-js/1.1.0/auth0-spa-js.production.js"></script>
-    <script src="public/js/app.js"></script>
+    <script src="js/app.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   </head>
